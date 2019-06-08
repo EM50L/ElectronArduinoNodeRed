fea4d6dbf958ef72b4bf97827287ecdf5582ac33
Sergio Blazquez Lopez <38666222+EM50L@users.noreply.github.com>
2019-06-08 10:24:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -30,6 +30,8 @@ console.log("__dirname:"+__dirname)
 console.log("Directorio UserDir:",userdir);
 console.log();
 
+let win; // subido de linea 145 a linea 33
+
 //_______________________________________________________________________________________________________
 // Codigo de https://nodered.org/docs/embedding
 console.log("cargo node-red https://nodered.org/docs/embedding");
@@ -52,11 +54,32 @@ red_app.use("/",express.static("public"));
 var settings = {
     verbose: true,
     httpAdminRoot:"/admin",
+    //adminAuth: {type: "credentials",users: [{username: "jejo",password: "$2a$08$T/zmFhFALnveBvdczC3ts.LEWeB9mJTDjVWqu3DdF7Xzf6Pu8MiVW",permissions: "*"},{username: "test",password: "$2a$08$VkDl5ql9uyfdbclDdE/Eh.FS.S/60qZb4n.Hyp9PIstlqtHhdG98K",permissions: "read"}]},
     httpNodeRoot: "/",
-    userDir: userdir,
+    userDir: userdir, // ver linea 24 Problema con el directorio a utilizar...
     flowFile: 'flows.json',
-    functionGlobalContext: { }    // enables global context
+    functionGlobalContext: { bcryptjs:require('bcryptjs'), fs:require('fs'), path:require('path'), os:require('os') ,electron:require('electron'),win}    // enables global context
 };
+
+
+var fs = require('fs')
+var pconf = path2.resolve(process.execPath, '../settings.js');
+try{
+	if (fs.existsSync(pconf)){
+		console.log("\nArchivo configuracion encontrado:"+pconf+"\n\n")
+		var config = require(pconf)
+		console.log(config)
+		console.log(config.functionGlobalContext)
+		// de momento solo  adminAuth https://nodered.org/docs/security
+		settings.adminAuth=config.adminAuth
+		settings.credentialSecret=config.credentialSecret
+		settings.editorTheme=config.editorTheme
+		//no funciona: ?????? !!! al parecer una vez compilado si que funciona
+		settings.functionGlobalContext=config.functionGlobalContext ; //https://nodered.org/docs/writing-functions.html#loading-additional-modules
+	}else{
+		console.log("\nArchivo configuracion NO encontrado:\n"+pconf+"\n\n")
+	}
+}catch(err){console.log(err)}
 
 console.log("Inicializa node-red RED.init()");
 RED.init(server,settings);// Inicializa runtime node-red 
@@ -78,31 +101,38 @@ RED.start().then(function() {
 }); 
 // Fin Codigo de https://nodered.org/docs/embedding
 
+
+//_______________________________________________________________________________________________________
 // Crea el menu 
 // Codigo https://www.christianengvall.se/electron-menu/)
+function acercade(item,fWin) {
+		const options = {type: 'info',message: " NodeJs      https://nodejs.org/\n ElectronJs  https://electronjs.org/\n Node-Red  https://nodered.org/ \n Jejo EM50L https://jejo.es ",buttons: ["OK"] };
+		dialog.showMessageBox(fWin,options,function(){})
+		}
+
 
 var template = [{
-	label: "Applicacion",submenu: [{label:'Acerca de...',role:'about'},{type:"separator"},{label:'Salir',role:'quit' }]
+	label: "Applicacion",submenu: [
+		{label:'Acerca de...',click(item,fWin){acercade(item,fWin)} },
+		{type:"separator"},{label:'Salir',role:'quit' }]
     },{
 	label: 'Node-RED',submenu: [
-		{label:'Dashboard',click(){win.loadURL("http://localhost:"+listenPort+"/ui/");}},
-		{label:'Editar Codigo',click(){win.loadURL("http://localhost:"+listenPort+urledit);}},	]
+		{label:'Dashboard',click(){win.loadURL("http://localhost:"+listenPort+"/ui/");}},//url);}},
+		{label:'Editar Codigo',click(){win.loadURL("http://localhost:"+listenPort+urledit);}}, ]
     },{
-	label:'Ver',submenu: [
-		{label: 'Recargar',accelerator:'CmdOrCtrl+R',
-			click(item,focusedWindow){if (focusedWindow)focusedWindow.reload();}
-		},{ label: 'Barra Desarrollador',
-			click(item, focusedWindow){if(focusedWindow)focusedWindow.webContents.toggleDevTools(); }
-		},{ type: 'separator' },{ role: 'resetzoom' 
-		},{ role: 'zoomin'    },{ role: 'zoomout' 
-		},{ type: 'separator' },{ role: 'togglefullscreen' 
-		},{ role: 'minimize' }	]
-    }/*,{
-	label:'Acerca de...',
-	click: function (item, focusedWindow) {dialog.showMessageBox({ message: "Jejo EM50L :-)",buttons: ["OK"] })}
-	}/* */
+	label:'Ver',submenu: [{
+		label: 'Recargar',accelerator:'CmdOrCtrl+R',click(item,fWin){if (fWin)fWin.reload();} },{ 
+		label: 'Barra Desarrollador',click(item,fWin){if(fWin)fWin.webContents.toggleDevTools();} 
+		},{ type: 'separator' },{ role: 'resetzoom'},{ role: 'zoomin'},{ role: 'zoomout' 
+		},{ type: 'separator' },{ role: 'togglefullscreen'},{ role: 'minimize' } ]
+    }
 ];
 const menu = Menu.buildFromTemplate(template)
+
+// otra forma posiblemente mas modular
+//var menu =new Menu()
+//https://github.com/electron/electron/blob/master/docs/api/menu.md
+//menu.append(new MenuItem({ label: 'MenuItem1', click() { console.log('item 1 clicked') } }))
 
 Menu.setApplicationMenu(menu)
 console.log("Estas ejecutando main.js 118 acabo de ejecutar:Menu.setApplicationMenu(menu)");
@@ -112,7 +142,7 @@ console.log("Estas ejecutando main.js 118 acabo de ejecutar:Menu.setApplicationM
 
 // Mantén una referencia global del objeto window, si no lo haces, la ventana 
 // se cerrará automáticamente cuando el objeto JavaScript sea eliminado por el recolector de basura.
-let win;
+//let win; // subido de linea 145 a linea 33
 
 function createWindow() {
 	console.log("Estas ejecutando main.js funcion createWindow() linea 133");
@@ -120,16 +150,9 @@ function createWindow() {
 
 	// Crea la ventana del navegador.
 	win = new BrowserWindow({
-		autoHideMenuBar: false,
-		webPreferences: {
-			nodeIntegration: false // sin esta linea no funciona el navegador en modo admin
-		//nodeIntegration: true
-		},
-		title: "Node-RED-Jejo",
-		fullscreenable: true,
-		titleBarStyle: "hidden",
-		width: 800,
-		height: 600,
+		webPreferences: { nodeIntegration: false },// sin esta linea no funciona el navegador en modo admin
+		title: "Node RED Em50L",//titleBarStyle: "hidden",
+		width: 800, height: 600,
 		icon: __dirname + "/nodered.png"
 	});
 	
@@ -172,10 +195,7 @@ app.on('window-all-closed', function () {
 app.on('activate', function() {
   // En macOS es común volver a crear una ventana en la aplicación cuando el
   // icono del dock es clicado y no hay otras ventanas abiertas.
-    if (win === null) {
-        createWindow();
-        win.loadURL("http://127.0.0.1:"+listenPort+url);
-    }
+  if (win === null) { createWindow(); win.loadURL("http://127.0.0.1:"+listenPort+url);}
 });
 
 // En este archivo puedes incluir el resto del código del proceso principal de
